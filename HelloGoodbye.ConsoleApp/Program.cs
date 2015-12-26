2b8893e30a084f1bc82750cf7788b79420465dc5
Matthew Butt <matthew@matthewbutt.com>
2015-12-26 22:47:34 +0000

    OK, it's a silly project, but it will do for checking CI.

---
+++ HelloGoodbye.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloGoodbye.ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            while (Console.ReadLine() != "Hello")
+            {
+                Console.WriteLine("Say that again.");
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
---
+++ HelloGoodbye.Tests/SmokeTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace HelloGoodbye.Tests
+{
+    [TestFixture]
+    public class SmokeTests
+    {
+        private Process _process;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var processStartInfo = new ProcessStartInfo(AppContext.BaseDirectory + "/HelloGoodbye.ConsoleApp.exe")
+            {
+                CreateNoWindow = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            _process = Process.Start(processStartInfo);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if(!_process.HasExited)
+                _process.Kill();
+            _process.Dispose();
+        }
+
+        [Test]
+        public void When_I_say_hello_you_say_goodbye()
+        {
+            _process.StandardInput.WriteLine("Hello");
+            Assert.That(_process.StandardOutput.ReadLine(), Is.EqualTo("Goodbye"));
+        }
+
+        [Test]
+        public void When_I_say_sausages_you_say_say_that_again()
+        {
+            _process.StandardInput.WriteLine("Sausages");
+            Assert.That(_process.StandardOutput.ReadLine(), Is.EqualTo("Say that again."));
+        }
+    }
+}
