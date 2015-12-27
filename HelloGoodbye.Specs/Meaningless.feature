3cca4783f5f3ec4b1cc70777e89050ada0b4ae75
Matthew Butt <matthew@matthewbutt.com>
2015-12-27 14:53:18 +0000

    Added a meaningless spec

---
+++ HelloGoodbye.Specs/Meaningless.feature
@@ -0,0 +1,8 @@
+﻿Feature: Meaningless
+	In order to test the CI system
+	As Matthew
+	I want to run a basic spec
+
+Scenario: Say hello and goodbye
+	When I say hello
+	Then you say goodbye
---
+++ HelloGoodbye.Specs/MeaninglessSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace HelloGoodbye.Specs
+{
+    [Binding]
+    public class MeaninglessSteps
+    {
+        [When(@"I say hello")]
+        public void WhenISayHello()
+        {
+            SayHello();
+        }
+
+        private void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        [Then(@"you say goodbye")]
+        public void ThenYouSayGoodbye()
+        {
+            var answer = SayGoodbye();
+            Assert.That(answer, Is.EqualTo("Goodbye"));
+        }
+
+        private string SayGoodbye()
+        {
+            return "Goodbye";
+        }
+    }
+}
